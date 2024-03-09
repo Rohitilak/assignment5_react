42d7fc25d134fbd362fefd82eb0e8b278372141a
Rohitilak <rohittilak086@gmail.com>
2024-03-09 16:58:51 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+// import CoursePlanner from './components2/CoursePlanner';
+import Main from './components/Main.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+      {/* <CoursePlanner/> */}
+    </div>
+  );
+}
+
+export default App;
